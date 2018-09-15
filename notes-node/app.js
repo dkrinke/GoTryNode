da2e90fc7a7058f8d57d8bee3f8028f272345361
dkrinke <krinkoid@gmail.com>
2018-09-15 13:05:08 -0400

    Clean up

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,24 +1,6 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js')
-
-// console.log(_.isString(true));
-// console.log(_.isString('Daniel'));
-
-var filteredArray = _.uniq(['Daniel',1,'Daniel',1,2,3,4]);
-console.log(filteredArray);
-
-// var user = os.userInfo();
-
-// var sum = notes.add(9,-2);
-
-// console.log(sum);
-
-// fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err) {
-// 	if (err) {
-// 		console.log('Unable to write to file');
-// 	}
-// });
