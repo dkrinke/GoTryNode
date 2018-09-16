3da8e8d36374e9f5f9515dc6c7ffe8467bb431b2
dkrinke <krinkoid@gmail.com>
2018-09-16 04:53:48 -0400

    Added yargs to node modules. weather-app now dynamically returns the address, lat, and long for a passed in address

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,40 @@
 const request = require('request');
 const fs = require('fs');
+const yargs = require('yargs');
+
+const argv = yargs
+	.options({
+		address: {
+			demand: true,
+			alias: 'a',
+			describe: 'Address to fetch weather for',
+			string: true
+		}
+	})
+	.help()
+	.alias('help', 'h')
+	.argv;
 
 var mapQuestKey = fs.readFileSync('mapquest-key.txt');
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-	url: `http://www.mapquestapi.com/geocoding/v1/address?key=${mapQuestKey}&location=1301%20lombard%20street%20philadelphia`,
+	url: `http://www.mapquestapi.com/geocoding/v1/address?key=${mapQuestKey}&location=${encodedAddress}`,
 	json: true
 }, (error, response, body) => {
-	console.log(body);
+
+	const location = body.results[0].locations[0];
+
+	const street = location.street;
+	const city = location.adminArea5;
+	const state = location.adminArea3;
+	const postalCode = location.postalCode;
+	const country = location.adminArea1
+
+	const latitude = location.latLng.lat;
+	const longitude = location.latLng.lng;
+
+	console.log(`Address: ${street}, ${city}, ${state} ${postalCode}, ${country}`);
+	console.log(`Latitude: ${latitude}`);
+	console.log(`Longitude: ${longitude}`);
 });
